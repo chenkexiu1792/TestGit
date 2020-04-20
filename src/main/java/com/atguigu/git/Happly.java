f2ff6764e35d9fa111e8b4f9d9e447d63bebecdc
dean <chenkexiu1792@163.com>
2020-04-20 11:25:55 +0800

    test conflict TestGit3 and TestGit4

--- src/main/java/com/atguigu/git/Happly.java
+++ src/main/java/com/atguigu/git/Happly.java
@@ -3,8 +3,9 @@ package com.atguigu.git;
 public class Happly {
 
 	public static void main(String[] args) {
-
 		System.out.println("TestGit");
+		System.out.println("TestGit-3");
+		System.out.println("TestGit-4");
 	}
 
 }
