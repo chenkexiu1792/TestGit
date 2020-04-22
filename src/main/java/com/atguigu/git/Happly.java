4d25df11409f1a709fd061115ed66ba407392ef8
dean <chenkexiu1792@163.com>
2020-04-22 14:17:51 +0800

    hot fix test

--- src/main/java/com/atguigu/git/Happly.java
+++ src/main/java/com/atguigu/git/Happly.java
@@ -4,6 +4,6 @@ public class Happly {
 
 	public static void main(String[] args) {
 		System.out.println("TestGit-----2");
-		System.out.println("TestGit");
+		System.out.println("hot fix ....");
 	}
 }
