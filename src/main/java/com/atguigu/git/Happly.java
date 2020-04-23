9c31e747959652cee3873f168a98a1b7f2bc7a5e
dean <chenkexiu1792@163.com>
2020-04-23 11:11:54 +0800

    test conflict first

--- src/main/java/com/atguigu/git/Happly.java
+++ src/main/java/com/atguigu/git/Happly.java
@@ -3,7 +3,6 @@ package com.atguigu.git;
 public class Happly {
 
 	public static void main(String[] args) {
-		System.out.println("TestGit-----2");
-		System.out.println("TestGit");
+		System.out.println("first");
 	}
 }
