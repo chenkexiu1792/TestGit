c5f6f6237c6d7af58fe242b6fcd213dd836dbc27
chenkexiu1792 <chenkexiu1792@163.com>
2020-05-14 15:38:29 +0800

    TV1  15:37 hotfix_2 cloud

--- src/main/java/com/atguigu/git/TV1.java
+++ src/main/java/com/atguigu/git/TV1.java
@@ -1,15 +1,12 @@
 package com.atguigu.git;
 
-/**
- * createDate: 2020年5月14日 
- * author: Herman
- */
 
 public class TV1 {
 
 	public static void main(String[] args) {
 		System.out.println("TV1  15:31 hotfix_2 ipofas");
 		System.out.println("TV1  15:35 hotfix_2 ipofas");
+		System.out.println("TV1  15:37 hotfix_2 cloud");
 	}
 
 }
