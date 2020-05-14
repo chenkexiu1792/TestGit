619de0ba84b7ca5b146ff5d04095881a1838f448
chenkexiu1792 <chenkexiu1792@163.com>
2020-05-14 15:25:33 +0800

    test version  15:19 hotfix_2 ipofas

---
+++ src/main/java/com/atguigu/git/TestVersion.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * createDate: 2020年5月14日 
+ * author: Herman
+ */
+
+public class TestVersion {
+
+	public static void main(String[] args) {
+		System.out.println("test version  15:19 hotfix_2 ipofas");
+	}
+
+}
