b575b925aee9e1709da619f310488469a0b0835b
chenkexiu1792 <chenkexiu1792@163.com>
2020-05-14 15:24:23 +0800

    test version  15:24 hotfix_2 cloud

--- src/main/java/com/atguigu/git/TestVersion.java
+++ src/main/java/com/atguigu/git/TestVersion.java
@@ -4,5 +4,6 @@ public class TestVersion {
 
 	public static void main(String[] args) {
 		System.out.println("test version  15:19 hotfix_2 cloud");
+		System.out.println("test version  15:24 hotfix_2 cloud");
 	}
 }
