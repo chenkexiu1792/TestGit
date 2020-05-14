7dde8b5c5318de2e107e6dd72232d7efb8e40b76
chenkexiu1792 <chenkexiu1792@163.com>
2020-05-14 15:42:50 +0800

    TV1  15:40 hotfix_2 ipofas

--- src/main/java/com/atguigu/git/TV1.java
+++ src/main/java/com/atguigu/git/TV1.java
@@ -7,6 +7,7 @@ public class TV1 {
 		System.out.println("TV1  15:31 hotfix_2 ipofas");
 		System.out.println("TV1  15:35 hotfix_2 ipofas");
 		System.out.println("TV1  15:37 hotfix_2 cloud");
+		System.out.println("TV1  15:40 hotfix_2 ipofas");
 	}
 
 }
