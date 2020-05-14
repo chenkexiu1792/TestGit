d77a33b4dcaf06cc4a06863b3ff3c8b0d5538797
chenkexiu1792 <chenkexiu1792@163.com>
2020-05-14 15:11:35 +0800

    modify TEMP

--- src/main/java/com/atguigu/git/TEMP.java
+++ src/main/java/com/atguigu/git/TEMP.java
@@ -4,7 +4,9 @@ public class TEMP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Local temp");
+		System.out.println("Local temp 1");
+		System.out.println("Local temp 2");
+		System.out.println("Local temp 3");
 	}
 
 }
