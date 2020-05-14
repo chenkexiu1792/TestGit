eb67f37ac0f4de3f7ee246983e216abbe52b553a
chenkexiu1792 <chenkexiu1792@163.com>
2020-05-14 15:26:48 +0800

    test version  15:24 hotfix_2 ipofas

--- src/main/java/com/atguigu/git/TestVersion.java
+++ src/main/java/com/atguigu/git/TestVersion.java
@@ -9,6 +9,7 @@ public class TestVersion {
 
 	public static void main(String[] args) {
 		System.out.println("test version  15:19 hotfix_2 ipofas");
+		System.out.println("test version  15:24 hotfix_2 ipofas");
 	}
 
 }
